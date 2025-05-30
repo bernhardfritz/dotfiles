b35adbf0bf8b4475b99a1b125965a3dffaf44eb6
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2025-05-30 18:48:21 +0200

    add docker to install script

--- install.sh
+++ install.sh
@@ -16,6 +16,28 @@ readarray -t packages < packages.txt
 sudo apt-get update
 sudo apt-get -y install "${packages[@]}"
 
+# Add Docker's official GPG key:
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install extra packages
+readarray -t extraPackages < extra-packages.txt
+sudo apt-get update
+sudo apt-get -y install "${extraPackages[@]}"
+
+# Manage Docker as a non-root user
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
 # Stow
 for directory in */; do
   stow "$directory"
