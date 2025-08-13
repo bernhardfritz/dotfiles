86eb5d640de88c0992d8652619a22e0f79459844
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2025-08-13 20:40:22 +0200

    add yt-dlp

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -2,3 +2,9 @@ alias code="flatpak run com.visualstudio.code"
 alias nvim="flatpak run io.neovim.nvim"
 alias vlc="flatpak run org.videolan.VLC"
 alias nvlc="flatpak run --command=nvlc org.videolan.VLC"
+alias yt-dl='docker run \
+                  --rm -i \
+                  -e PGID=$(id -g) \
+                  -e PUID=$(id -u) \
+                  -v "$(pwd)":/workdir:rw \
+                  mikenye/youtube-dl'
